ff875e42114ff099ad38d99c11f61729439c1ef5
vdsfvd <kambleaditya490@gmail.com>
2025-09-27 08:07:15 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
 import app from "./app.js";
 import connectDB from "./db/db.js";
 import dotenv from "dotenv"
-
 dotenv.config()
-const PORT = process.env.PORT || 3000;
-
+const PORT = 3000 || process.env.PORT ;
 connectDB();
 
 app.listen(PORT, () => {
