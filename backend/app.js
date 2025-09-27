155905b4c56d169d1183dc07b7f9e4dd9cee9162
vdsfvd <kambleaditya490@gmail.com>
2025-09-27 14:38:27 +0530

    backend things

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,7 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin : process.env.FRONTEND_URL || "https://snackhot.onrender.com",
-    credentials : true
-}))
+app.use(cors())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/food" , foodroutes)
